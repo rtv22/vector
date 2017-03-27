643c9b26a8f12f6b5b422480083ba0e633237a6c
rtv22 <rtvcojigor@gmail.com>
2017-03-27 13:12:25 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,19 @@ SCENARIO("vector init", "[init]") {
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
 }
+
+SCENARIO("params", "[init with params]") {
+	int init = 3;
+	vector_t vector (init);
+	REQUIRE(vector.size() == 3);
+	REQUIRE(vector.capacity() == 6);
+}
+
+SCENARIO("copy", "[copy]") 
+{
+	vector_t A(1, 1);
+	vector_t B(A);
+	REQUIRE(B.size() == 1);
+	REQUIRE(B.capacity() == 1);
+}
+
